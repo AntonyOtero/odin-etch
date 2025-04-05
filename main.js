4813a9d13381105ebf8ac1ffc345426e25f2e2a4
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-05 07:47:03 -0400

    feat: Add button to generate the grid given user input

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-const GRID_CONTAINER = document.querySelector(".grid-container");
+const getGridSize = () => {
+    const gridSize = prompt("Enter a grid size (0-100): ");
 
-//Grid size will be squared
-let gridSizeSq = 16;
+    return gridSize;
+}
 
 const generateGrid = (gridSize) => {
-    for (i = 0; i < gridSizeSq; i++) {
-        for (j = 0; j < gridSizeSq; j++) {
+    const GRID_CONTAINER = document.querySelector(".grid-container");
+
+    if (GRID_CONTAINER) {
+        document.body.removeChild(GRID_CONTAINER);
+    }
+
+    let newGridContainer = document.createElement("div");
+    newGridContainer.classList.add("grid-container");
+    document.body.appendChild(newGridContainer);
+
+    for (i = 0; i < gridSize; i++) {
+        for (j = 0; j < gridSize; j++) {
             let newGridUnit = document.createElement("div");
             newGridUnit.classList.add("grid-unit", "debug");
-            GRID_CONTAINER.appendChild(newGridUnit);
+            newGridContainer.appendChild(newGridUnit);
         }
     }
+
+    const GRID_UNITS = [...document.querySelectorAll(".grid-unit")];
+
+    GRID_UNITS.forEach(unit => {
+        unit.addEventListener("mouseover", e => updateUnitColor(e.target));
+    });
+}
+
+const generateNewGrid = () => {
+    generateGrid(getGridSize());
 }
 
 const updateUnitColor = (unit, color="#212121") => {
     unit.style.backgroundColor = color;
 }
-
-generateGrid(16);
-
-const GRID_UNITS = [...document.querySelectorAll(".grid-unit")];
-
-GRID_UNITS.forEach(unit => {
-    unit.addEventListener("mouseover", e => updateUnitColor(e.target));
-});
