5efc89b6f564252f2bef81e192c7f35504e089eb
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-05 07:17:30 -0400

    refactor: Grid generation and unit coloring are now functions

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const GRID_CONTAINER = document.querySelector(".grid-container");
 
-//Grid size squared
+//Grid size will be squared
 let gridSizeSq = 16;
 
-
-for (i = 0; i < gridSizeSq; i++) {
-    for (j = 0; j < gridSizeSq; j++) {
-        let newGridUnit = document.createElement("div");
-        newGridUnit.classList.add("grid-unit", "debug");
-        GRID_CONTAINER.appendChild(newGridUnit);
+const generateGrid = (gridSize) => {
+    for (i = 0; i < gridSizeSq; i++) {
+        for (j = 0; j < gridSizeSq; j++) {
+            let newGridUnit = document.createElement("div");
+            newGridUnit.classList.add("grid-unit", "debug");
+            GRID_CONTAINER.appendChild(newGridUnit);
+        }
     }
 }
+
+const updateUnitColor = (unit, color="#212121") => {
+    unit.style.backgroundColor = color;
+}
+
+generateGrid(16);
 
 const GRID_UNITS = [...document.querySelectorAll(".grid-unit")];
 
 GRID_UNITS.forEach(unit => {
-    unit.addEventListener("mouseover", e => e.target.style.backgroundColor = "#212121");
+    unit.addEventListener("mouseover", e => updateUnitColor(e.target));
 });
