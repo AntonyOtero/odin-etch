ca6be70f6819baa7e95d44c9a335dc0267c41701
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-05 07:08:40 -0400

    feat: Hovering over a grid unit now updates it's color

--- main.js
+++ main.js
@@ -11,3 +11,9 @@ for (i = 0; i < gridSizeSq; i++) {
         GRID_CONTAINER.appendChild(newGridUnit);
     }
 }
+
+const GRID_UNITS = [...document.querySelectorAll(".grid-unit")];
+
+GRID_UNITS.forEach(unit => {
+    unit.addEventListener("mouseover", e => e.target.style.backgroundColor = "#212121");
+});
